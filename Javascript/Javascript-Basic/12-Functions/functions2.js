b410b84e97d7a5cab0c990880936fb252283f908
mustafahincal <mustafahncal@gmail.com>
2023-01-16 23:44:18 +0300

    arguments

--- Javascript/Javascript-Basic/12-Functions/functions2.js
+++ Javascript/Javascript-Basic/12-Functions/functions2.js
@@ -1,53 +1,50 @@
 //! Function Declarations
 
 function sum1(a, b) {
-    var c = a + b;
-    return c;
+  var c = a + b;
+  return c;
 }
 console.log(sum1(10, 20));
 
-
 //! Function Expression
 
-let sum2 = function(a, b) {
-    var c = a + b;
-    return c;
-}
+let sum2 = function (a, b) {
+  var c = a + b;
+  return c;
+};
 
 console.log(sum2(10, 20));
 
 //? console.log(sum2(10,20,50,40)); 50 ve 40 degerlerini görmez.
 //? console.log(sum2(10)); NaN degeri döndürür , buna güzel bir ayar çekebiliriz.
 
-let sum3 = function(a, b) {
+let sum3 = function (a, b) {
+  if (typeof a === "undefined") {
+    a = 0;
+  }
+  if (typeof b === "undefined") {
+    b = 0;
+  }
 
-    if (typeof a === "undefined") {
-        a = 0;
-    }
-    if (typeof b === "undefined") {
-        b = 0;
-    }
-
-    var c = a + b;
-    return c;
-}
+  var c = a + b;
+  return c;
+};
 console.log(sum3(10)); //? bu sefer NaN döndürmez b yi 0 alır ve  10 döndürür direk.
 
 //* ES6 Default Parameters
 
 //? Burda başlangıc da 0 a eşitliyoruz eğer fonsksiyon'a  a'nın yerine bi şey gönderirlerse 0'ı ezer ve o değer olur ama gönderilmezse a 0 kalır.
 
-let sum4 = function(a = 0, b = 0) {
-    var c = a + b;
-    return c;
-}
+let sum4 = function (a = 0, b = 0) {
+  var c = a + b;
+  return c;
+};
 console.log(sum4(20));
-
 
 //* Arguments
 
 function args() {
-    console.log(arguments);
+  console.log(arguments);
 }
 args(10, 20, 50, 60, 25);
 
@@ -55,12 +52,22 @@ args(10, 20, 50, 60, 25);
 
 //? biz arguments'i böyle kullanabiliriz , bellekde bir yerde duruyor o sayılar hep ama arguments ile onları ortaya cıkarıyoruz.
 
-
 function sumAll() {
-    var total = 0;
-    for (let i = 0; i < arguments.length; i++) {
-        total += arguments[i];
-    }
-    return total;
+  var total = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    total += arguments[i];
+  }
+  return total;
 }
 console.log(sumAll(10, 20, 50, 30, 40));
+
+function test() {
+  console.log(arguments); // [Arguments] { '0': 1, '1': 2, '2': 3, '3': 4, '4': 5 }
+}
+test(1, 2, 3, 4, 5);
+
+function test2(...numbers) {
+  console.log(numbers); // [ 6, 7, 8, 9 ]
+  console.log(...numbers); // 6, 7, 8, 9
+}
+test2(6, 7, 8, 9);
