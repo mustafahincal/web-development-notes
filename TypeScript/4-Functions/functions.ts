3e7e8db1de2db577bd8618f111f5f359611ea609
mustafahincal <mustafahncal@gmail.com>
2023-01-22 19:37:26 +0300

    bug

--- TypeScript/4-Functions/functions.ts
+++ TypeScript/4-Functions/functions.ts
@@ -1,24 +1,23 @@
-function getAverage(a:number,b:number,c:number): string{
-    const result = (a+b+c)/3;
-    return "result : "+result;
+function getAverage(a: number, b: number, c: number): string {
+  const result = (a + b + c) / 3;
+  return "result : " + result;
 }
 
-getAverage(10,20,30);
+getAverage(10, 20, 30);
 //* normal bir toplama fonksiyonuna sayı dışında bir şey göndermek hata olur : ama ben bu hatayı runtime'da değilde compiletime'da göreyimki hatamı düzelteyim. O yüzden typescript type tanımlamasını kullanabilirim.
 
-
-function getAverage2(a:number,b:number,c?:number): string{
-    let total = a+b;
-    let count=2;
-    if(typeof c !== "undefined"){
-        total+=c;
-        count++;
-    }
-    const result = total/count;
-    return "result : "+result;
+function getAverage2(a: number, b: number, c?: number): string {
+  let total = a + b;
+  let count = 2;
+  if (typeof c !== "undefined") {
+    total += c;
+    count++;
+  }
+  const result = total / count;
+  return "result : " + result;
 }
 
-getAverage2(10,20);
+getAverage2(10, 20);
 
 //* 3 parametre isteyen bir fonksiyona 2 parametre gönderirsem hata verir ancak c?:number diye tanımlarsam 2 parametre gönderdiğim durumda hata vermez.
 
@@ -26,21 +25,18 @@ getAverage2(10,20);
 
 //* ne kadar sayı göndereceğim belli değilse
 
-function getAverage3(...numbers: number[]): string{
-    let total = 0;
-    let count=0;
-    
-    for(let i=0 ; i<numbers.length;i++){
-        total+=numbers[i];
-        count++;
-    }
-    const result = total/count;
-    return "result : "+result;
+function getAverage3(...numbers: number[]): string {
+  let total = 0;
+  let count = 0;
+
+  for (let i = 0; i < numbers.length; i++) {
+    total += numbers[i];
+    count++;
+  }
+  const result = total / count;
+  return "result : " + result;
 }
 
-getAverage3(10,20,30,50,60);
+getAverage3(10, 20, 30, 50, 60);
 
 //* dönüş tipini void yaparsam bir şey return etmeyeceğim demek anlamına gelir ve return yazma durumunda hata verdirir.
-
-
-
